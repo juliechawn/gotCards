1a571ed81c8b5218309bca51c12a91b61c1bc180
Julie C  Hawn <juliechawn@gmail.com>
2019-04-17 17:43:10 -0400

    more changes

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import "./card.css";
+import {Stark} from "./images/stark.png"
 
 class Card extends Component {
   constructor(props) {
@@ -11,77 +12,107 @@ class Card extends Component {
     this.renderBack = this.renderBack.bind(this);
   }
 
+  getHouseF() {
+    switch (this.props.house) {
+      case "Stark":
+        return "stark-f";
+      case "Targaryen":
+        return "targaryen-f";
+      case "Lannister":
+        return "lannister-f";
+      default:
+    }
+  }
+
+  getHouseB() {
+    switch (this.props.house) {
+      case "Stark":
+        return "stark-b";
+      case "Targaryen":
+        return "targaryen-b";
+      case "Lannister":
+        return "lannister-b";
+      default:
+    }
+  }
+
   renderFront() {
-    return(
-    <div
-      className="card-front"
-      value={this.props.id}
-      onClick={() => this.setState({ toggle: !this.state.toggle })}
-    >
-      <img className="char-img" alt=" " src={this.props.image}  />
-      <div className="char-title">
-        <span className="char-name">{this.props.name}</span>
+    return (
+      <div
+        className={`card-front ${this.getHouseF()}`}
+        value={this.props.id}
+        onClick={() => this.setState({ toggle: !this.state.toggle })}
+      >
+        <img className="char-img" alt=" " src={this.props.image} />
+        <div className="char-title">
+          <span className="char-name">{this.props.name}</span>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 
   renderBack() {
-    return(
-    <div className="card-back" onClick={() => this.setState({ toggle: !this.state.toggle })}>
-      <div className="char-title">
-        <div className="title">{this.props.name}</div>
-        {/* <span>{this.props.status}</span> */}
-        <div className="nickname">{this.props.nickname}</div>
+    return (
+      <div
+        className={`card-back ${this.getHouseB()}`}
+        onClick={() => this.setState({ toggle: !this.state.toggle })}
+      >
+        <div className="char-title">
+          <div className="title">{this.props.name}</div>
+          {/* <span>{this.props.status}</span> */}
+          <div className="nickname">{this.props.nickname}</div>
+        </div>
+        <div className="char-bio">
+          <table>
+            <thead>
+              <tr>
+                <th>HOUSE</th>
+                <th>ALLEGIANCE</th>
+                <th>BIRTHPLACE</th>
+                <th>AGE</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{this.props.house}</td>
+                <td>{this.props.allegiance}</td>
+                <td>{this.props.birthplace}</td>
+                <td>{this.props.age}</td>
+              </tr>
+            </tbody>
+            <thead>
+              <tr>
+                <th>SPOUSE</th>
+                <th>BEST FRIEND</th>
+                <th>PET</th>
+                <th>WEAPON</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  {this.props.spouse}
+                  <span className="spouse-status">
+                    {" "}{this.props.spousestatus}
+                  </span>
+                </td>
+                <td>{this.props.bestfriend}</td>
+                <td>{this.props.pet}</td>
+                <td>{this.props.weapon}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="char-quote">
+          <span>"{this.props.quote}"</span>
+        </div>
       </div>
-      <div className="char-bio">
-      <table>
-      <thead>
-      <tr>
-        <th>HOUSE</th>
-        <th>ALLEGIANCE</th> 
-        <th>BIRTHPLACE</th>
-        <th>AGE</th>   
-      </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{this.props.house}</td>
-          <td>{this.props.allegiance}</td>
-          <td>{this.props.birthplace}</td>
-          <td>{this.props.age}</td>
-        </tr>
-      </tbody>
-      <thead>
-      <tr>
-        <th>SPOUSE</th>
-        <th>BEST FRIEND</th>
-        <th>PET</th> 
-        <th>WEAPON</th>
-      </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{this.props.spouse} <span className="spouse-status">{this.props.spousestatus}</span></td>
-          <td>{this.props.bestfriend}</td>
-          <td>{this.props.pet}</td>
-          <td>{this.props.weapon}</td>
-        </tr>
-      </tbody>
-    </table>
-      </div>
-      <div className="char-quote">
-        <span>"{this.props.quote}"</span>
-      </div>
-    </div>
-    )
+    );
   }
 
   render() {
     return (
-      <div>
-        {this.state.toggle ? this.renderFront() : this.renderBack()}
-      </div>
+      <div>{this.state.toggle ? this.renderFront() : this.renderBack()}</div>
     );
   }
 }
